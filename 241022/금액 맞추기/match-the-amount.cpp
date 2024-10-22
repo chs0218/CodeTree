aa53d938834eae413e20830e193ca870b9324efe
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-10-22 22:00:52 +0900

    Title: 금액 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/금액 맞추기/match-the-amount.cpp
+++ 241022/금액 맞추기/match-the-amount.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
+
 int coins[50][10];
 int dp[50][1001];   // n명의 사람이 돈을 냈을 때, 합이 k가 되는 경우의 수
 int main() {
     int n, m, k;
     cin >> n >> m >> k;
 
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         int nCoin;
         cin >> nCoin;
-        for(int j = 0; j < nCoin; ++j)
+        for (int j = 0; j < nCoin; ++j)
         {
             cin >> coins[i][j];
         }
@@ -18,38 +19,41 @@ int main() {
 
     // Initialize
     // 첫번째 사람이 들고 있는 동전으로 초기화
-
-    for(int i = 0; i < n; ++i)
+    
+    for (int i = 0; i < n; ++i)
     {
         dp[i][0] = 1;
     }
 
-    for(int i = 0; i < 10; ++i)
+    for (int i = 0; i < 10; ++i)
     {
         // 동전 가격이 0이면 탐색 완료
-        if(coins[0][i] == 0) break;
+        if (coins[0][i] == 0) break;
         dp[0][coins[0][i]] += 1;
     }
 
-    for(int i = 1; i < n; ++i)
+    for (int i = 1; i < n; ++i)
     {
-        for(int j = 0; j < 10; ++j)
+        for (int j = 0; j < 10; ++j)
         {
-            // 동전 가격이 0이면 탐색 완료
-            if(coins[i][j] == 0) break;
-            for(int k = 0; k <= 1000; ++k)
+            if (coins[i][j] == 0) break;
+            dp[i][coins[i][j]] += 1;
+        }
+
+        for (int j = 1; j <= 1000; ++j)
+        {
+            dp[i][j] += dp[i - 1][j];
+            for (int k = 0; k < 10; ++k)
             {
-                // 경우의 수가 있으면
-                if(dp[i - 1][k] > 0)
+                if (coins[i][k] == 0) break;
+                if (dp[i - 1][j] > 0 && j + coins[i][k] <= 1000)
                 {
-                    if(k + coins[i][j] <= 1000)
-                        dp[i][k + coins[i][j]] = (dp[i - 1][k] + 1) % 10007;
-                    dp[i][k] = dp[i][k] + dp[i - 1][k];
+                    dp[i][j + coins[i][k]] += dp[i - 1][j];
                 }
             }
         }
     }
 
-    cout << dp[n - 1][k];
+    cout << dp[n - 1][k] % 10007;
     return 0;
 }
