3d4f87276920c0d9830c985cb936429e7881f703
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-10-23 02:42:41 +0900

    Title: 화면에 출력, Time: 16ms, Memory: 1MB, Status: Passed - CodeTree

--- 241023/화면에 출력/output-to-screen.cpp
+++ 241023/화면에 출력/output-to-screen.cpp
@@ -2,11 +2,13 @@
 #include <queue>
 #include <tuple>
 using namespace std;
+bool bVisited[1001][1001];
 int main() {
     int s;
     cin >> s;
 
     priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+    bVisited[1][0] = true;
     pq.push({ 0, 1, 0 });
 
     int ans = 1e9;
@@ -19,22 +21,30 @@ int main() {
         int nCharacter = get<1>(cur);
         int nClipBoard = get<2>(cur);
 
-        if (nCharacter < 1 && nClipBoard == 0)
-            continue;
-
-        if (nCharacter == s)
+        if (s == nCharacter)
         {
             ans = nValue;
             break;
         }
 
-        if (s > nCharacter)
+        if (3 < nCharacter && !bVisited[nCharacter - 1][nClipBoard])
         {
-            if (nClipBoard != 0)
-                pq.push({ nValue + 1, nCharacter + nClipBoard, nClipBoard });
+            bVisited[nCharacter - 1][nClipBoard] = true;
+            pq.push({ nValue + 1, nCharacter - 1, nClipBoard });
+        }
+
+        if(nCharacter < s && !bVisited[nCharacter][nCharacter])
+        {
+            bVisited[nCharacter][nCharacter] = true;
             pq.push({ nValue + 1, nCharacter, nCharacter });
         }
-        pq.push({ nValue + 1, nCharacter - 1, nClipBoard });
+
+        if (nCharacter < s && nClipBoard != 0 && 
+            nCharacter + nClipBoard <= 1000 && !bVisited[nCharacter + nClipBoard][nClipBoard])
+        {
+            bVisited[nCharacter + nClipBoard][nClipBoard] = true;
+            pq.push({ nValue + 1, nCharacter + nClipBoard, nClipBoard });
+        }
     }
     cout << ans;
     return 0;
