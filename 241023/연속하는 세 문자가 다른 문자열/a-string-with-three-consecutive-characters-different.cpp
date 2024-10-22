7512768b5656873f4b0128c4d88852eddfa139d9
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-10-23 01:12:03 +0900

    Title: 연속하는 세 문자가 다른 문자열, Time: 35ms, Memory: 44MB, Status: Passed - CodeTree

--- 241023/연속하는 세 문자가 다른 문자열/a-string-with-three-consecutive-characters-different.cpp
+++ 241023/연속하는 세 문자가 다른 문자열/a-string-with-three-consecutive-characters-different.cpp
@@ -1,41 +1,49 @@
 #include <iostream>
-using namespace std;
-int n;
-int nCharacter[6];
-int MakeWord(int nLastLastCharacter, int nLastCharacter)
-{
-    bool bUsedAllcharacter = true;
-    for(int i = 1; i <= n; ++i)
-    {
-        if(nCharacter[i] > 0) 
-        {
-            bUsedAllcharacter = false;
-            break;
-        }
-    }
-    if(bUsedAllcharacter) return 1;
+#include <memory.h>
 
-    int ret = 0;
-    for(int i = 1; i <= n; ++i)
-    {
-        if(nCharacter[i] > 0 && i != nLastCharacter && i != nLastLastCharacter)
-        {
-            nCharacter[i] -= 1;
-            ret += MakeWord(nLastCharacter, i);
-            nCharacter[i] += 1;
+#define MAX_N 5
+#define MAX_M 10
+
+using namespace std;
+
+// 변수 선언
+int n;
+int freq[MAX_N];
+
+// dp[s1, s2, s3, s4, s5, c1, c2] : 
+// 각 문자가 s1, .., s5개 남았고 최근 연속한 2개의 문자 번호가 순서대로 c1, c2라 했을 때
+// 가능한 서로 다른 가짓수
+long long dp[MAX_M + 1][MAX_M + 1][MAX_M + 1][MAX_M + 1][MAX_M + 1][MAX_N + 1][MAX_N + 1];
+
+long long Counting(int c1, int c2) {
+    long long &ret = dp[freq[0]][freq[1]][freq[2]][freq[3]][freq[4]][c1][c2];
+    if(ret != -1)
+        return ret;
+    
+    // 초기조건은 freq 값이 전부 0인 경우입니다.
+    if(freq[0] + freq[1] + freq[2] + freq[3] + freq[4] == 0)
+        return 1;
+    
+    ret = 0;
+    for(int i = 0; i < n; i++)
+        if(freq[i] > 0 && i != c1 && i != c2) {
+            freq[i]--;
+            ret += Counting(c2, i);
+            freq[i]++;
         }
-    }
+    
     return ret;
 }
+
 int main() {
+    // 입력:
     cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> freq[i];
 
-    for(int i = 1; i <= n; ++i)
-    {
-        cin >> nCharacter[i];
-    }
-
-    int ans = MakeWord(0, 0);
-    cout << ans;
+    // dp값을 -1로 초기화합니다.
+    memset(dp, -1, sizeof(dp));
+    
+    cout << Counting(n, n);
     return 0;
 }
