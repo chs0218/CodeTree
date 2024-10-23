56398a538e917008fbb497d35a640775ea495621
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-10-24 00:55:26 +0900

    Title: 가장 많이 겹치는 구간, Time: 92ms, Memory: 1MB, Status: Passed - CodeTree

--- 241024/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
+++ 241024/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
-
+int check[200001];
 int main() {
     int n;
     cin >> n;
 
-    vector<pair<int, int>> v(n * 2);
     for(int i = 0; i < n; ++i)
     {
         int l, r;
         cin >> l >> r;
-        v.push_back({l, +1});
-        v.push_back({r, -1});
+        ++check[l];
+        --check[r];
     }
 
-    sort(v.begin(), v.end());
-
     int ans = -1;
+    int nOverlap = 0;
     for(int i = 1; i <= 200000; ++i)
     {
-        int temp = 0;
-        for(int j = 0; j < v.size(); ++j)
-        {
-            if(v[j].first > i) break;
-            temp += v[j].second;
-        }
-        ans = max(ans, temp);
+        nOverlap += check[i];
+        ans = max(ans, nOverlap);
     }
     cout << ans;
     return 0;
